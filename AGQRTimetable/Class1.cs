da2984c70a5e7828ce27c80d7c7801a627718c1f
Maisu <mystasly48@gmail.com>
2020-03-19 00:37:51 +0900

    Add sample project

--- AGQRTimetable/Class1.cs
+++ AGQRTimetable/Class1.cs
@@ -4,9 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AGQRTimetable
-{
-    public class Class1
-    {
-    }
+namespace AGQRTimetable {
+  public class AGQR {
+
+  }
 }
